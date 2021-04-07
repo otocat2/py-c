9fece64b29adf579005f32c9666dfd9e7246c5fd
otocat2 <lchuang@tkenonprofit.org>
2021-04-07 08:59:23 -0400

    testing 1 2 3

---
+++ cinthya.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+series1 = pd.Series([1,2,3,4])
+
+print(series1)
+
+# csvfile = "c:\temp\temp.csv"
+# with open(csvfile, newline='') as f:
+#     reader = csv.reader(f, delimiter=',')
+#     for row in reader:
+#         for compare_row in reader:
+#             if row == compare_row
+#                 print(row,'is equal to',compare_row)
+#             else:
+#                 print(row,'is not equal to',compare_row)
